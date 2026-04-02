cea14969706e576d7e43bb38c48afbe73952e898
ByteyWolf <235580007+ByteyWolf@users.noreply.github.com>
2026-04-02 21:34:27 +0300

    skeleton

--- client/src/BUMPProtocol.java
+++ client/src/BUMPProtocol.java
@@ -1,5 +1,41 @@
 import javax.microedition.io.*;
+import java.io.*;
 
 public class BUMPProtocol {
-    
+    public static final int STATE_DISCONNECTED = 0;
+    public static final int STATE_CONNECTING = 1;
+    public static final int STATE_CONNECTED = 2;
+
+    public StreamConnection conn = null;
+    public volatile byte currentState = STATE_DISCONNECTED;
+    public Exception lastException = null;
+
+    public byte connect(String destination) throws IOException {
+        if (currentState != STATE_DISCONNECTED) {
+            return 0;
+        }
+        lastException = null;
+        currentState = STATE_CONNECTING;
+        conn = (StreamConnection) Connector.open(destination);
+        new Thread(new Reader()).start();
+        new Thread(new Writer()).start();
+        return 1;
+    }
+
+    private class Reader implements Runnable {
+        public void run() {
+            try {
+                // todo: here we receive
+            } catch (IOException e) {
+                currentState = STATE_DISCONNECTED;
+                lastException = e;
+            }
+        }
+    }
+
+    private class Writer implements Runnable {
+        public void run() {
+            // todo: here we send
+        }
+    }
 }
